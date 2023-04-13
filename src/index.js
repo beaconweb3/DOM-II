7b61bcd37b6c24dbd00dca6a5948c1a09becdce6
Talon Revis <113617774+beaconweb3@users.noreply.github.com>
2023-04-13 17:30:48 -0500

    changed all p elements using .from  with mapfn parameter

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './less/index.less'
 const navlinks = document.querySelectorAll('.nav-link');
 navlinks.forEach( a => a.addEventListener('mouseover', (event) => {a.style = 'color: blue'}));
 
+const firstPar = Array.from(document.getElementsByTagName('p'), p => p.textContent = "I Changed all the text content with one line");
+console.log(firstPar);
