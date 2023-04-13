782e805063f6b9672322106e0ffabbd99dbf5905
Talon Revis <113617774+beaconweb3@users.noreply.github.com>
2023-04-13 15:55:23 -0500

    Added mouseover listener to anchor tags

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './less/index.less'
+const navlinks = document.querySelectorAll('.nav-link');
+navlinks.forEach( a => a.addEventListener('mouseover', (event) => {a.style = 'color: blue'}));
 
-// Your code goes here!
